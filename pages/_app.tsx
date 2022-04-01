041c9b79065b36125bd78b02ea65e4b6d71ec8f0
김성수 <fereverv@openbase.co.kr>
2022-04-01 15:56:34 +0900

    스타일 설정

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/app';
+import React from 'react';
+import GlobalStyle from '../styles/GlobalStyle';
+
+const app = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps}/>
+    </>
+  )
+};
+
+export default app;
