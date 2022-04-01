72764c441e183c3e37a06e748ba337ff862c0797
김성수 <fereverv@openbase.co.kr>
2022-04-01 16:52:25 +0900

    airbnb 이미지 파일 추가

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { css } from "styled-components";
+
+const Container = css`
+
+
+`;
+
+const Header: React.FC = (props) => {
+  return (
+    <Container>
+      <div className="header-logo-wrapper">
+        
+      </div>
+    </Container>
+  );
+}
+
+export default Header;
